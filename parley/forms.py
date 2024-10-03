c035f5bc5e9717ef4234e5887ccfff2b6b38d778
Benjamin Bengfort <benjamin@bengfort.com>
2024-10-03 13:57:59 -0500

    uploader complete

--- parley/forms.py
+++ parley/forms.py
@@ -17,9 +17,145 @@ Forms for handling and validating user input.
 ## Imports
 ##########################################################################
 
-from django import forms
+import os
+import json
+import tempfile
 
+from django import forms
+from collections import defaultdict
+from parley.exceptions import ParlanceUploadError
+from django.core.exceptions import ValidationError
+from parley.models import LLM, Evaluation, Prompt, Response
+
+
+##########################################################################
+## Fields
+##########################################################################
+
+class MultipleFileInput(forms.ClearableFileInput):
+
+    allow_multiple_selected = True
+
+
+class MultipleFileField(forms.FileField):
+
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput())
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = [single_file_clean(data, initial)]
+        return result
+
+
+##########################################################################
+## Helpers
+##########################################################################
+
+class UploaderCounts(object):
+
+    def __init__(self):
+        # Stores files: objects: operation: count
+        self.counts = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
+
+    def increment(self, fname, obj, created):
+        if created:
+            self.created(fname, obj.__class__.__name__)
+        else:
+            self.updated(fname, obj.__class__.__name__)
+
+    def created(self, fname, otype):
+        self.counts[fname][otype]["created"] += 1
+
+    def updated(self, fname, otype):
+        self.counts[fname][otype]['updated'] += 1
+
+    def html(self):
+        lines = [
+            f'<h4 class="alert-heading">Upload Complete: {len(self.counts)} File(s) Processed</h4>'
+        ]
+
+        row = 0
+        for fname, ocounts in self.counts.items():
+            # Make sure every file has an hr accept the last one.
+            if row != 0:
+                lines.append("<hr />")
+            row += 1
+
+            lines.append(f'<p class="mb-1"><code>{fname}</code> results:</p>\n<ul class="my-0">')
+            for otype, c in ocounts.items():
+                lines.append(f'<li><code>{otype}</code>: {c["created"]} created, {c["updated"]} updated')
+            lines.append('</ul>')
+
+        return "\n".join(lines)
+
+
+##########################################################################
+## Forms
+##########################################################################
 
 class Uploader(forms.Form):
 
-    jsonl = forms.FileField(allow_empty_file=False)
+    jsonl = MultipleFileField(allow_empty_file=False)
+
+    def clean_jsonl(self):
+        files = self.cleaned_data["jsonl"]
+        for f in files:
+            # Ensure these files are JSON lines files
+            _, ext = os.path.splitext(f.name)
+            if ext not in {".jsonl", ".jsonlines"}:
+                raise ValidationError("must specify only .jsonlines or .jsonl files")
+        return files
+
+    def handle_upload(self):
+        counts = UploaderCounts()
+        files = self.cleaned_data["jsonl"]
+        with tempfile.TemporaryDirectory() as td:
+            for f in files:
+                self.handle_uploaded_file(td, f, counts)
+        return counts
+
+    def handle_uploaded_file(self, td, f, counts):
+        # Write the file to a temporary file on disk
+        path = self.write_temporary_file(td, f)
+
+        # Read the temporary file and handle contents
+        for r, row in self.read_jsonlines(path):
+            if 'type' not in row:
+                raise ParlanceUploadError(
+                    f"missing type field on line {r} of {f.name}"
+                )
+
+            rtype = {
+                'llm': LLM,
+                'evaluation': Evaluation,
+                'prompt': Prompt,
+                'response': Response,
+            }.get(row.pop('type').strip().lower(), None)
+
+            if rtype is None:
+                raise ParlanceUploadError(
+                    f"unknown type \"{row['type']}\" on line {r} of {f.name}"
+                )
+
+            counts.increment(f.name, *rtype.objects.get_or_create(**row))
+
+    def read_jsonlines(self, path):
+        with open(path, 'r') as f:
+            for i, line in enumerate(f.readlines()):
+                try:
+                    yield i+1, json.loads(line)
+                except json.JSONDecodeError:
+                    fname = os.path.basename(path)
+                    raise ParlanceUploadError(f"invalid json on line {i+1} of {fname}")
+
+    def write_temporary_file(self, td, f):
+        path = os.path.join(td, f.name)
+        with open(path, 'wb') as fh:
+            for chunk in f.chunks():
+                fh.write(chunk)
+        return path
--- parley/views.py
+++ parley/views.py
@@ -17,8 +17,14 @@ Parley views and controllers.
 ## Imports
 ##########################################################################
 
+from django.db import transaction
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.utils.safestring import mark_safe
 from django.views.generic.edit import FormView
+
 from parley.forms import Uploader
+from parley.exceptions import ParlanceUploadError
 
 
 ##########################################################################
@@ -27,21 +33,31 @@ from parley.forms import Uploader
 
 class UploaderFormView(FormView):
 
-    ready = False
     template_name = "upload.html"
     form_class = Uploader
+    success_url = reverse_lazy("upload")
 
     def form_valid(self, form):
-        if not self.ready:
-            raise Exception("not implemented yet")
-        return super().form_valid(form)
+        # Handle the uploaded data stored in the form
+        try:
+            counts = form.handle_upload()
+        except ParlanceUploadError as e:
+            # If an error occurs, stop processing and show the error to the user.
+            form.add_error(None, str(e))
+            return self.form_invalid(form)
 
-    def form_invalid(self, form):
-        if not self.ready:
-            raise Exception("form invalid")
+        # Create the success message with the counts
+        messages.success(self.request, mark_safe(counts.html()))
         return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["page_id"] = "upload"
         return context
+
+    @transaction.atomic
+    def post(self, *args, **kwargs):
+        """
+        Ensures that all database operations during a request are all or nothing.
+        """
+        return super().post(*args, **kwargs)
