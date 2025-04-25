fa573085e74f03ae502ad8ae056b251cba2f3bd7
Patrick Deziel <deziel.patrick@gmail.com>
2025-04-25 12:16:21 -0500

    remove unused imports

--- parley/views.py
+++ parley/views.py
@@ -23,7 +23,6 @@ import json
 from collections import defaultdict
 
 from django.views import View
-from django.shortcuts import render
 from django.db import transaction
 from django.contrib import messages
 from django.urls import reverse_lazy
